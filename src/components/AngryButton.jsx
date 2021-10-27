260c34c20166b873996e8082ed74726669204797
AJ <aj@smartnvm.com>
2021-10-26 23:17:48 -0400

    - add app components

---
+++ src/components/AngryButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react'; //optional
+
+function AngryButton(){
+  return (
+<button className="AngryButton">
+  {/* When the threshold is not reached */}
+  <span>Don't click me too much! </span>
+  {/* When the threshold is not reached */}
+  <span>Rawr!</span>
+</button>
+
+  );
+}
+
+export default AngryButton;
