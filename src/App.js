fc849518a3542918a4ef328b2e26836e4bfbe465
AJ <aj@smartnvm.com>
2021-10-27 02:13:53 -0400

    - add dark mode state lift up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import AngryButton from "./components/AngryButton";
-import CounterButton from "./components/CounterButton";
-import LightSwitchButton from "./components/LightSwitchButton";
-import TextRepeaterButton from "./components/TextRepeaterButton";
+import AngryButton from './components/AngryButton';
+import CounterButton from './components/CounterButton';
+import LightSwitchButton from './components/LightSwitchButton';
+import TextRepeaterButton from './components/TextRepeaterButton';
 
-import { useState } from "react";
+import { useState } from 'react';
 
 import './App.css';
 
 function App() {
-  const [light, setLight] = useState('off')
+  const [light, setLight] = useState('off');
+  
+  const switchLight = () => setLight((light === "on") ? "off" : "on");
+  const dark = (light === 'off') ? 'dark' : '';
+
+  const props = { light, switchLight }
   
   return (
-    <div className={`App`}>
+    <div className={`App ${dark}`}>
       <h1>Fancy Buttons!</h1>
       <section>
         <AngryButton />
         <CounterButton />
-        <LightSwitchButton  light={light} setLight={setLight}/>
+        <LightSwitchButton {...props} />
         <TextRepeaterButton />
       </section>
     </div>
--- src/components/LightSwitchButton.jsx
+++ src/components/LightSwitchButton.jsx
@@ -2,21 +2,12 @@ import React from 'react'; //optional
 
 
 function LightSwitchButton(props){
-  const { light, setLight } = props;
+  const { light,switchLight} = props;
   
-
-	const handleClick = () => {
-		if (light === 'off') {
-			setLight('on');
-		} else {
-			setLight('off');
-		}
-	};
-
 
   return (
     <button className="LightSwitchButton"
-    onClick = {handleClick}>
+    onClick = {switchLight}>
     {light === "on" && <span><i>💡</i> I'm on!</span>}
     {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
   </button>
